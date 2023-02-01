c21adfff9eedcdced49f5f622452969237267eaa
Stefan Filipovic <stefan.filipovic05@gmail.com>
2023-02-01 09:47:28 +0100

    score and hitbox

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 let ship = document.querySelector("#ship")
 let game = document.querySelector('#game')
+let scorebox = document.querySelector("#scorebox")
 let asteroidi = []
+let score = 0
+let speed = 1000   
 
 ship.style.left = '50vw';
 ship.style.top = '90vh';
@@ -8,15 +11,17 @@ let pos = ship.offsetLeft;
 
 window.addEventListener("keydown", (e)=>{
     if(e.key == "ArrowLeft" && pos > 680) {
-        ship.style.left = (pos-20)+"px";
+        ship.style.left = (pos-40)+"px";
         pos = ship.offsetLeft;
     }else if (e.key == "ArrowRight" && pos < 1163) {
-        ship.style.left = pos+20+"px";
+        ship.style.left = pos+40+"px";
         pos = ship.offsetLeft;
     }
 })
 
-setInterval(() => {
+setTimeout(a(), speed)
+
+function a() {
     let magicanAsteroid = document.createElement("img")
     magicanAsteroid.src = 'asteroid.png';
     magicanAsteroid.style.top = '10vh';
@@ -25,25 +30,28 @@ setInterval(() => {
     magicanAsteroid.className = 'asteroid';
     game.appendChild(magicanAsteroid);
     asteroidi.push(magicanAsteroid);
-
-}, 3000);
+    speed-=10;
+    setTimeout(a, speed)
+}
 
 setInterval(() => {
     asteroidi.forEach((e)=>{
         let ws = e.offsetTop
         e.style.top = ws+5+"px"
-        if(e.offsetTop > ship.offsetTop - 40) {
+        if(e.offsetTop > ship.offsetTop - 40 && e.offsetTop < ship.offsetTop+50) {
             console.log("aaasssa")
-            if(e.offsetLeft < ship.offsetLeft+40 && e.offsetLeft > ship.offsetLeft-40) {
+            if(e.offsetLeft < ship.offsetLeft+40 && e.offsetLeft > ship.offsetLeft-100) {
                 console.log("Aas")
                 game.remove();
-                document.body.innerHTML = "you dieded"
+                document.body.innerHTML = "you dieded, final score: " + score
             }
         }
         if(ws >1000)
         {
             e.remove()
             console.log("del")
+            score++
+            scorebox.innerHTML = "score:" + score
         }
     })
 }, 10);
